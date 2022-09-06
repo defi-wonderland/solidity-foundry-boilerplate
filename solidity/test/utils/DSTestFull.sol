e17ac6109dced401a78a52a607ead5af29f2e8a0
eugenioclrc <eugenioclrc@gmail.com>
2022-09-06 11:02:58 -0300

    refactor: use named imports, console2 its not need when use PRBTest

--- solidity/test/utils/DSTestFull.sol
+++ solidity/test/utils/DSTestFull.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4 <0.9.0;
 
-import 'forge-std/console2.sol';
+import {console} from 'forge-std/console.sol';
 import {PRBTest} from 'prb/test/PRBTest.sol';
 
 contract DSTestFull is PRBTest {
