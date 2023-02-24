68c10a381d207f7a182a34ece612c46d30557f5b
0xRaccoon <raccoon@defi.sucks>
2023-02-24 17:26:23 -0300

    ci: add solhint wonderland config and fix warns
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- solidity/scripts/Deploy.sol
+++ solidity/scripts/Deploy.sol
@@ -6,25 +6,25 @@ import {Greeter} from 'contracts/Greeter.sol';
 import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 
 abstract contract Deploy is Script {
-    function _deploy(string memory greeting, IERC20 token) internal {
-        vm.startBroadcast();
-        new Greeter(greeting, token);
-        vm.stopBroadcast();
-    }
+  function _deploy(string memory greeting, IERC20 token) internal {
+    vm.startBroadcast();
+    new Greeter(greeting, token);
+    vm.stopBroadcast();
+  }
 }
 
 contract DeployMainnet is Deploy {
-    function run() external {
-        IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+  function run() external {
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
-        _deploy('some real greeting', weth);
-    }
+    _deploy('some real greeting', weth);
+  }
 }
 
 contract DeployRinkeby is Deploy {
-    function run() external {
-        IERC20 weth = IERC20(0xDf032Bc4B9dC2782Bb09352007D4C57B75160B15);
+  function run() external {
+    IERC20 weth = IERC20(0xDf032Bc4B9dC2782Bb09352007D4C57B75160B15);
 
-        _deploy('some test greeting', weth);
-    }
+    _deploy('some test greeting', weth);
+  }
 }
--- solidity/test/e2e/Greeter.t.sol
+++ solidity/test/e2e/Greeter.t.sol
@@ -4,13 +4,13 @@ pragma solidity >=0.8.4 <0.9.0;
 import {CommonE2EBase} from 'test/e2e/Common.sol';
 
 contract E2EGreeter is CommonE2EBase {
-    function test_Greet() public {
-        uint256 whaleBalance = dai.balanceOf(daiWhale);
+  function test_Greet() public {
+    uint256 _whaleBalance = _dai.balanceOf(_daiWhale);
 
-        vm.prank(daiWhale);
-        (string memory greeting, uint256 balance) = greeter.greet();
+    vm.prank(_daiWhale);
+    (string memory _greeting, uint256 _balance) = _greeter.greet();
 
-        assertEq(whaleBalance, balance);
-        assertEq(initialGreeting, greeting);
-    }
+    assertEq(_whaleBalance, _balance);
+    assertEq(_initialGreeting, _greeting);
+  }
 }
