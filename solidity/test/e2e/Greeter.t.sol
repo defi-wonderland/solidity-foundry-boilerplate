21bc01153a515193f005fc93bc2b1eca2c6aeafc
Gas One Cent <86567384+gas1cent@users.noreply.github.com>
2023-05-11 00:54:43 +0400

    feat: use a newer Solidity version

--- solidity/test/e2e/Greeter.t.sol
+++ solidity/test/e2e/Greeter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.4 <0.9.0;
+pragma solidity >=0.8.18 <0.9.0;
 
 import {CommonE2EBase} from 'test/e2e/Common.sol';
 
--- solidity/test/utils/DSTestFull.sol
+++ solidity/test/utils/DSTestFull.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4 <0.9.0;
+pragma solidity >=0.8.18 <0.9.0;
 
 import {console} from 'forge-std/console.sol';
 import {PRBTest} from 'prb/test/PRBTest.sol';
