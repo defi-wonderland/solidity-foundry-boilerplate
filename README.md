5d38fe8b42424eed91a1aa1e8bb98b66ae2c93bf
0xDiscotech <0xdiscotech@defi.sucks>
2023-06-02 19:47:03 -0300

    feat: set mainnet as network on common for e2e tests, and updated addresses
    * fix: added specification to run fonudryup in case the install commands

--- README.md
+++ README.md
@@ -31,8 +31,8 @@
 ## Setup
 
 1. Install Foundry by following the instructions from [their repository](https://github.com/foundry-rs/foundry#installation).
-2. Copy the `.env.example` file to `.env` and fill in the variables
-3. Install the dependencies by running : `yarn install && forge install`
+2. Copy the `.env.example` file to `.env` and fill in the variables.
+3. Install the dependencies by running: `yarn install && forge install`. In case there is an error with the commands, run `foundryup` and try them again.
 
 ## Build
 
--- solidity/test/e2e/Common.sol
+++ solidity/test/e2e/Common.sol
@@ -8,17 +8,17 @@ import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 import {Greeter, IGreeter} from 'contracts/Greeter.sol';
 
 contract CommonE2EBase is DSTestFull {
-  uint256 internal constant _FORK_BLOCK = 8_945_216;
+  uint256 internal constant _FORK_BLOCK = 15_452_788;
 
   string internal _initialGreeting = 'hola';
   address internal _user = _label('user');
   address internal _owner = _label('owner');
-  address internal _daiWhale = 0x93e39f67f79B448ABd58fC7Ef813c55636c4510f;
-  IERC20 internal _dai = IERC20(0x65a5ba240CBd7fD75700836b683ba95EBb2F32bd);
+  address internal _daiWhale = 0x42f8CA49E88A8fd8F0bfA2C739e648468b8f9dec;
+  IERC20 internal _dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
   IGreeter internal _greeter;
 
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('goerli'), _FORK_BLOCK);
+    vm.createSelectFork(vm.rpcUrl('mainnet'), _FORK_BLOCK);
     vm.prank(_owner);
     _greeter = new Greeter(_initialGreeting, _dai);
   }
