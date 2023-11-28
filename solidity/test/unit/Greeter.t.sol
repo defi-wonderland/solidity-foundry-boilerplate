2d2a24fc311b4853f9a952b0eb1ed2a8d18eae29
Dristpunk <dristpunk@gmail.com>
2023-11-28 18:02:36 +0300

    fix: add vm.etch to mock contract

--- solidity/test/unit/Greeter.t.sol
+++ solidity/test/unit/Greeter.t.sol
@@ -7,12 +7,14 @@ import {Greeter, IGreeter} from 'contracts/Greeter.sol';
 
 abstract contract Base is Test {
   address internal _owner = makeAddr('owner');
+
   IERC20 internal _token = IERC20(makeAddr('token'));
   string internal _initialGreeting = 'hola';
   bytes32 internal _emptyString = keccak256(bytes(''));
   Greeter internal _greeter;
 
   function setUp() public virtual {
+    vm.etch(address(_token), new bytes(0x1)); // etch bytecode to avoid address collision problems
     vm.prank(_owner);
     _greeter = new Greeter(_initialGreeting, _token);
   }
