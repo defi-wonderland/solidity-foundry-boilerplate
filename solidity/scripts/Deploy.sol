e97d8ca671f1f417810c90174acc29749facc3a9
0xDiscotech <0xdiscotech@defi.sucks>
2024-01-02 13:38:34 -0300

    feat: add sort imports true on forge fmt
    * chore: sorted imports on contracts using forge fmt

--- solidity/scripts/Deploy.sol
+++ solidity/scripts/Deploy.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.19;
 
-import {Script} from 'forge-std/Script.sol';
 import {Greeter} from 'contracts/Greeter.sol';
+import {Script} from 'forge-std/Script.sol';
 import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 
 abstract contract Deploy is Script {
