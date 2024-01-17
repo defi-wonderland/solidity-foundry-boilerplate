dc52f62f1c662f04b3e98be78bd0e1d0fbca7893
0xdeo <0xdeo@defi.sucks>
2024-01-17 16:01:09 -0500

    fix: goerli references changed to sepolia

--- solidity/scripts/Deploy.sol
+++ solidity/scripts/Deploy.sol
@@ -21,7 +21,7 @@ contract DeployMainnet is Deploy {
   }
 }
 
-contract DeployGoerli is Deploy {
+contract DeploySepolia is Deploy {
   function run() external {
     IERC20 weth = IERC20(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
 
