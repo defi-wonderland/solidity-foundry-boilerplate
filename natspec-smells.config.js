97adec5d2e350568add24589e145a9766f2ea867
dristpunk <107591874+dristpunk@users.noreply.github.com>
2024-02-01 15:53:10 +0300

    feat: add natspec-smells (#56)
    Co-authored-by: Gas <86567384+gas1cent@users.noreply.github.com>

---
+++ natspec-smells.config.js
@@ -0,0 +1,9 @@
+/**
+ * List of supported options: https://github.com/defi-wonderland/natspec-smells?tab=readme-ov-file#options
+ */
+
+/** @type {import('@defi-wonderland/natspec-smells').Config} */
+module.exports = {
+  include: 'solidity',
+  exclude: ['solidity/(test|scripts)/**/*.sol'],
+};
