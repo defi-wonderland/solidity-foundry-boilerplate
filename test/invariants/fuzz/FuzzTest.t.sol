f55bbb8723a0eb73734da92cda18c412154374cf
teddy <teddy@defi.sucks>
2025-08-21 14:59:46 -0300

    chore: remove advanced testing from default boilerplate (#114)

--- test/invariants/fuzz/FuzzTest.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.23;
-
-import {GreeterGuidedHandlers} from './handlers/guided/Greeter.t.sol';
-import {GreeterUnguidedHandlers} from './handlers/unguided/Greeter.t.sol';
-import {GreeterProperties} from './properties/Greeter.t.sol';
-
-contract FuzzTest is GreeterGuidedHandlers, GreeterUnguidedHandlers, GreeterProperties {}
